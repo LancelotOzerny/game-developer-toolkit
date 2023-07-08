3a194da9714ce71b24a5d782f39f5b5723090a9e
Максим Беляков <belyakov@intervolga.ru>
2023-07-08 10:19:38 +0300

    Создал скрипт по работе с гитом и внес в него основные команды - пустые функции, которые подлежат реализации

---
+++ scripts/GitController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GitController : MonoBehaviour
+{
+    [MenuItem("Tools/Git/Init")]
+    public static void Init()
+    {
+
+    }
+
+    [MenuItem("Tools/Git/Add")]
+    public static void Add()
+    {
+
+    }
+
+    [MenuItem("Tools/Git/Open Folder")]
+    public static void OpenFolder()
+    {
+
+    }
+
+    [MenuItem("Tools/Git/Rollback")]
+    public static void Rollback()
+    {
+
+    }
+
+    [MenuItem("Tools/Git/Status")]
+    public static void Status()
+    {
+
+    }
+
+    [MenuItem("Tools/Git/Shortlog 10")]
+    public static void Shortlog10()
+    {
+
+    }
+
+    [MenuItem("Tools/Git/Shortlog 20")]
+    public static void Shortlog20()
+    {
+
+    }
+
+}
