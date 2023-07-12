6ab477df2477077ab9fe2254e9d6aed33ca020cc
Максим Беляков <belyakov@intervolga.ru>
2023-07-12 23:16:07 +0300

    Реализовал замену сообщения последнего коммита

--- Editor/GitWindow.cs
+++ Editor/GitWindow.cs
@@ -1,74 +1,94 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-
 using UnityEditor;
 using UnityEngine;
 
+namespace Lancy
+{
+    public class GitWindow : EditorWindow
+    {
+        private GitView _view = new GitView();
+        private GUIContent[] _tabs = {
+            new GUIContent("Commits"),
+            new GUIContent("Branches"),
+            new GUIContent("Remote"),
+        };
+        private int _currentTab = 0;
+
+        [MenuItem("Tools/Git Editor")]
+        public static void Show()
+        {
+            EditorWindow.GetWindow(typeof(GitWindow));
+        }
+
+        string commitText = "";
+        bool isAmend = false;
+
+        private void OnGUI()
+        {
+            // Repository is exist
+            if (GitController.Instance.Exists() == false)
+            {
+                if (_view.Init()) GitController.Instance.Init();
+                return;
+            }
+
+            // Repository is not exist
+            _currentTab = GUILayout.Toolbar(_currentTab, _tabs);
+            switch(_tabs[_currentTab].text.ToLower())
+            {
+                case "commits":
+                    if (_view.Commit(ref commitText, ref isAmend))
+                    {
+                        if (commitText == string.Empty)
+                        {
+                            Debug.LogWarning("Enter commit message");
+                            return;
+                        }
+
+                        if (isAmend)
+                        {
+                            GitController.Instance.Amend(commitText);
+                            isAmend = false;
+                            return;
+                        }
+
+                        GitController.Instance.Commit(commitText);
+                        commitText = string.Empty;
+                    }
+                    break;
+            }
+        }
+    }
+
+    public class GitView
+    {
+        public bool Init()
+        {
+            return GUILayout.Button("Инициализировать репозиторий", GUILayout.Height(50 ));
+        }
+
+        public bool Commit(ref string commitText, ref bool amend)
+        {
+            GUILayout.Label("Commit Area", GUILayout.Height(50));
+            commitText = GUILayout.TextArea(commitText, GUILayout.Height(100));
+            amend = GUILayout.Toggle(amend, "Amend", GUILayout.Height(50));
+            return GUILayout.Button("Commit", GUILayout.Height(50));
+        }
+    }
+}
+
 public class GitWindow : EditorWindow
 {
-    // GIT WORK VARIABLES
-    private string commitText = "";
-    private bool amend = false;
-
-    
     // STANDART METHODS
-    [MenuItem("Tools/Git Editor")]
+    [MenuItem("Tools/Old Git Editor")]
     public static void Show()
     {
         EditorWindow.GetWindow(typeof(GitWindow));
     }
+
     private void OnGUI()
     {
-        if (GitController.Instance.Exists())
-        {
-            AreaIndexing();
-            AreaPush();
-            AreaCommit();
-        }
-        else
-        {
-            AreaInit();
-        }
-    }
-
-
-    // VIEW METHOD
-    private void AreaInit()
-    {
-        GUILayout.Space(10);
-        if (GUILayout.Button("Git initialize", GUILayout.Height(50)))
-        {
-            try
-            {
-                GitController.Instance.Init();
-            }
-            catch (Exception e)
-            {
-                bool check = Directory.Exists(Directory.GetCurrentDirectory() + "\\.git\\info");
-                Debug.Log(check ? "True" : "False");
-            }
-
-            GitController.Instance.AddAll();
-        }
-    }
-    private void AreaCommit()
-    {
-        GUILayout.Label("Commit Area", GUILayout.Height(50));
-        GUILayout.Space(10);
-        commitText = GUILayout.TextArea(commitText, GUILayout.Height(50));
-        GUILayout.Space(10);
-        amend = GUILayout.Toggle(amend, "Amend");
-        GUILayout.Space(10);
-        if (GUILayout.Button("Commit", GUILayout.Height(50)))
-        {
-            if (commitText.Length > 0)
-            {
-                GitController.Instance.Commit(commitText);
-                commitText = "";
-                amend = false;
-            }
-        }
+        AreaIndexing();
+        AreaPush();
     }
     private void AreaPush()
     {
